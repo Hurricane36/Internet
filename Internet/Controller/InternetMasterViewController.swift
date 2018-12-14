519fdbab966e4206403d8769435901309cb0dc43
Hurricane36 <43070976+Hurricane36@users.noreply.github.com>
2018-12-14 09:44:28 -0700

    created a master controller

---
+++ Internet/Controller/InternetMasterViewController.swift
@@ -0,0 +1,92 @@
+//
+//  InternetMasterViewController.swift
+//  Internet
+//
+//  Created by Tolman, Caiden on 12/14/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class InternetMasterViewController: UITableViewController
+{
+    
+    private weak var internetDetail : InternetDetailViewController? = nil
+    private lazy var internetTopics : [String] = [String]()
+    private lazy var addresses : [String] = [String]()
+    
+    private func setupDetailContents() -> Void
+    {
+            internetTopics =
+                [
+                    "Interet Definitions ‼️😸",
+                    "Standard Search Engine 📱",
+                    "AP CSP 👷‍♂️",
+                    "Canyons District🏫",
+                    "CTEC🎓",
+                    "Social Media😈"
+                ]
+        addresses =
+            [
+                "https://www.google.com",
+                "https://www.google.com",
+                "https://canyons.instructure.com/courses/1256267",
+                "http://www.canyonsdistrict.org/",
+                "https://ctec.canyonsdistrict.org/",
+                "https://www.instagram.com/"
+            ]
+        
+            if let splitView = splitViewController
+            {
+                let currentControllers = splitView.viewControllers
+                internetDetail = currentControllers[0] as? InternetDetailViewController
+            }
+    }
+   public override func viewDidLoad() -> Void
+   {
+        super.viewDidLoad()
+    
+        setupDetailContents()
+    self.clearsSelectionOnViewWillAppear = false
+    
+        if let split = splitViewController
+        {
+            let controllers = split.viewControllers
+            internetDetail = (controllers[controllers.count-1] as! UINavigationController).topViewController as?
+                InternetDetailViewController
+            
+        }
+    }
+    public override func viewWillAppear(_ animated: Bool) -> Void
+    {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        
+        super.viewWillAppear(animated)
+    }
+    
+    
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return addresses.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    
+        let rowText = internetTopics[indexPath.row]
+        cell.textLabel!.text = rowText
+        return cell
+    }
+    
+    public override func tableView(_ tabkeView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        internetDetail?.detailAddress = addresses[indexPath.row]
+        internetDetail?.detailTitle = internetTopics[indexPath.row]
+        if (internetDetail != nil)
+        {
+            splitViewController?.showDetailViewController(internetDetail!, sender: nil)
+        }
+    }
+}
